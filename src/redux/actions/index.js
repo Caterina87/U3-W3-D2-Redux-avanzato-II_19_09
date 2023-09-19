e7f141d3761e1a745d216e3a0af24ecf7b776df2
Caterina <137883905+Caterina87@users.noreply.github.com>
2023-09-19 14:24:09 +0200

    first commit

---
+++ src/redux/actions/index.js
@@ -0,0 +1,32 @@
+export const ADD_TO_FAVOURITE = "ADD_TO_FAVOURITE";
+export const REMOVE_FROM_FAVOURITE = "REMOVE_FROM_FAVOURITE";
+export const GET_JOBS = "GET_JOBS";
+
+export const addToFavoritesAction = companyName => ({
+  type: ADD_TO_FAVOURITE,
+  payload: companyName
+});
+
+export const removeFromFavoritesAction = companyName => ({
+  type: REMOVE_FROM_FAVOURITE,
+  payload: companyName
+});
+
+export const getJobsAction = (baseEndpoint, query) => {
+  return async (dispatch, getState) => {
+    try {
+      const response = await fetch(baseEndpoint + query + "&limit=20");
+      if (response.ok) {
+        const { data } = await response.json();
+
+        //   setJobs(data);
+        dispatch({ type: GET_JOBS, payload: data });
+        console.log("DATA", data);
+      } else {
+        alert("Error fetching results");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
